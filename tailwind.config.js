476a65326eab703b5e8c72144ff8b4be608469b6
martine1grace <martinegrace16@gmail.com>
2022-10-09 10:28:33 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: [
+    "*.{html, js}"
+  ],
   theme: {
-    screens:{
-      sm:'480px',
-      md:'768px',
-      lg:'976px',
-      xl:'1440px'
-
-    },
     extend: {},
   },
   plugins: [],
